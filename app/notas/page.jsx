8850cfd137e4e049b10b5672b6fac027da812d95
Oco <92460234+nachodecores@users.noreply.github.com>
2023-08-13 20:08:57 -0300

    setup 1.2

---
+++ app/notas/page.jsx
@@ -0,0 +1,28 @@
+export default function Notas() {
+  return (
+    <>
+      <h3>About Us</h3>
+      <h2>Comsumir API de Wikipedia</h2>
+      <p>
+        Programmers should be considered like musicians or writers: they take
+        elements of a certain language and put them toghether to create an
+        artwork. Who do you believe are the 100 most talented programmers in the
+        whole history?
+      </p>
+      Que los usuarios vayan poniendo sus candidatos. La idea es poner un form
+      en el que los usuario vayan postulando sus programadores. Al final del
+      form hay un checkbox con el que el usuario inicia sesión con Google,
+      GitHub o Facebook. Sólo un check box.
+      <p>
+        Creemos que los distinguidos postulados, debido a sus contribuciones a
+        la disciplina, se ganó la distinción de estar en Wikipedia
+      </p>
+      <p>Form:</p>
+      <p>Candidato (poner filtro con el array de candidatos)</p>
+      <p>
+        Radio button, o checkbox o loguitos para iniciar sesión con Google, FB,
+        o Github
+      </p>
+    </>
+  );
+}
